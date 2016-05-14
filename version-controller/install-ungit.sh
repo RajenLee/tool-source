a360a4cac1c2220e05731fde23f1edff622dbddb
Rajen <rajen.lyz@gmail.com>
2016-05-14 09:17:51 +0800

    update script: add USER & notes

--- version-controller/install-ungit.sh
+++ version-controller/install-ungit.sh
@@ -5,20 +5,28 @@
 # created_date: May 13th 2016
 # updated_date: May 13th 2016
 
-# Declare: 
-
 # Prerequisites
-#     need install: nodejs, npm
+#     TO Install: NVM, Node.js, NPM
+#          NVM: Node version Manager, use it to install Node.js
+#          Node.js: a JavaScript runtime
+#          NPM: Node.js' package ecosystem, installed together with node.js
+
+# Declare: 
+#     nvm's installing get from : https://github.com/creationix/nvm
 
 USER=`whoami`
 MYPATH=`cat /etc/passwd|grep $USER|cut -d: -f 6`
 
 IS_CURL=`dpkg -l|grep curl|grep ii|cut -d ' ' -f 3|grep -x curl`
 
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Need run as root, ple try sudo..."
+    exit 1
+fi
+
 #install curl
 if [[ ! "$IS_CRUL" ]]; then
-	#sudo apt-get install -y curl
-    echo "===install curl===$?"
+    sudo apt-get install -y curl
     if [[ "$?" -ne 0 ]]; then
         echo "Error: install curl failed..."
         exit 1
@@ -30,16 +38,18 @@ cd /tmp
 curl -o google.html https://www.google.com
 
 if [[ ! -f google.html ]]; then
-    echo "Warning: Connect to google failed, if installing ungit failed, "
-    echo "         ple check the network.... "
+    echo "Warning: Connecting to google failed, it may lead to failure... "
+    echo "         If so, ple check the network.... "
     exit 0
 fi
 
 
-#install nvm
+#install nvm 
 cd $MYPATH
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
 
+# use nvm to install node.js
 nvm install stable
 
+# use npm to install ungit
 npm install -g ungit
